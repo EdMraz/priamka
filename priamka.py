e8007c34eedba1fafa631f3782500ff0022a76bf
EdMraz <eduard.mraz05@gmail.com>
2023-03-08 23:13:30 +0100

    initial commit

---
+++ priamka.py
@@ -0,0 +1,30 @@
+from PIL import Image
+
+pic = Image.new("RGB", (300,300), 'white')
+pixels = pic.load()
+A = (290, 150)
+B = (30, 100)
+
+#ak to je zvislá čiara
+if A[0]==B[0]:
+    for y in range(A[1],B[1]):
+        x = A[0]
+        pixels[x, y] = (0, 0, 0)
+
+#ak to ide z ľava do prava
+elif A[0]<B[0]:
+    k = (B[1]-A[1])/(B[0]-A[0])
+    q = A[1]-k*A[0]
+    for x in range(A[0],B[0]):
+        y = int(k*x+q)
+        pixels[x, y]=(0,0,0)
+
+#ak to ide z prava do ľava
+elif A[0]>B[0]:
+    k = (A[1] - B[1]) / (A[0] - B[0])
+    q = B[1] - k * B[0]
+    for x in range(B[0], A[0]):
+        y = int(k * x + q)
+        pixels[x, y] = (0, 0, 0)
+
+pic.show()
