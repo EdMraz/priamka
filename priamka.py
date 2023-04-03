0d0bd2d96da7af60d8fd4fe03c05cfa0e357624f
EdMraz <eduard.mraz05@gmail.com>
2023-04-03 19:43:49 +0200

    fixed code

--- priamka.py
+++ priamka.py
@@ -2,13 +2,23 @@ from PIL import Image
 
 pic = Image.new("RGB", (300,300), 'white')
 pixels = pic.load()
-A = (290, 150)
-B = (30, 100)
+A = (10, 150)
+B = (10, 15)
 
 #ak to je zvislá čiara
-if A[0]==B[0]:
+if A[0]==B[0] and A[1]<B[1]:
     for y in range(A[1],B[1]):
         x = A[0]
+        pixels[x, y] = (0, 0, 0)
+
+if A[0]==B[0] and A[1]>B[1]:
+    for y in range(B[1],A[1]):
+        x = A[0]
+        pixels[x, y] = (0, 0, 0)
+
+if A[0]==B[0] and A[1]==B[1]:
+        x = A[0]
+        y = A[1]
         pixels[x, y] = (0, 0, 0)
 
 #ak to ide z ľava do prava
